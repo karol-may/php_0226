89c23eb502484fb7b8a47f6b3ac8240215f700c2
Karol May <karol@imay.pl>
2026-01-11 11:46:33 +0100

    formatowanie formularza

--- src/prawy.php
+++ src/prawy.php
@@ -6,16 +6,7 @@ $conn = new mysqli($servername, $username, $password,"default_database",$port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-?>
-<form>
-    ID:<input type="number" placeholder="<?= $id ?>" readonly>
-    Email: <input type="email" placeholder="<?= $email ?>">
-    Data: <input type="text" placeholder="<?= $created_at ?>" readonly>
-    <input type="submit" value="Zmień hasło">
-    <input type="submit" value="Zapisz">
-</form>
 
-<?php
 if ($id) {
 
     $sql = "SELECT * FROM users WHERE id = $id";
@@ -24,15 +15,18 @@ if ($id) {
 
     if ($result->num_rows === 1){
         $row = $result->fetch_object(User::class);
-        echo "<p>
-            <h1>ID:{$row->id}</h1>
-            <i>{$row->email}</i>
-            <b>{$row->getCreatedAt()}</b>
-            <pre>";
-              var_dump($row);
-        echo "</pre>
-        </p>";
+        ?>
+        <form method="POST">
+            <label class="form-label" for="id">ID:</label><input class="form-control" id="id" name="id" type="number" placeholder="<?= $row->id?>" readonly>
+             <label class="form-label" for="email"> Email: <input  class="form-control" id="email" name="email" type="email" placeholder="<?= $row->email ?>">
+             <label class="form-label" for="createdAt">Data: <input class="form-control" id="createdAt" name="createdAt" type="date" placeholder="<?= $row->getCreatedAt() ?>" readonly>
+           <div class="mt-2">
+            <input class="btn btn-danger" type="button" value="Zmień hasło">
+            <input class="btn btn-primary"  type="submit" value="Zapisz">
+            </div>
+        </form>
 
+        <?php
     }
 
 }
